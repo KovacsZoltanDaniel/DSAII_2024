14d05422235469eaab123c146e28dd223e9d8654
KovacsZoltanDaniel <kovacs.zoltan@student.ms.sapientia.ro>
2024-03-17 23:58:03 +0200

    lab-04

---
+++ lab-04/headers/backtracking.h
@@ -0,0 +1,27 @@
+//
+// Created by kovac on 2024. 03. 17..
+//
+
+#ifndef LAB_02_BACKTRACKING_H
+#define LAB_02_BACKTRACKING_H
+
+#endif //LAB_02_BACKTRACKING_H
+
+#include <iostream>
+
+using namespace std;
+
+void Bt(int *x, int n, int k);
+void kiir(int *x, int n);
+void kerekpar(int *x,int n, int k);
+void descartes(int *x, int n, int k);
+void permutacio(int *x, int n, int k);
+bool igeretes(int *x, int k);
+bool ellenorizKombinacio(int * array, int index);
+void Kombinacio(int * array, int n, int k, int index);
+bool ellenorizVariacio(int * array, int index);
+void Variacio(int * array, int n, int k, int index);
+void kiralyno(int *x, int n, int k);
+bool igereteskiralyno(int *x, int k);
+
+
--- lab-04/main.cpp
+++ lab-04/main.cpp
@@ -1,6 +1,21 @@
-#include <iostream>
+#include "Headers/backtracking.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int n = 3;
+    int *x = new int[n];
+    //Bt(x,n,0);
+    //kerekpar(x,n,1);
+    //descartes(x,n,1);
+    //permutacio(x,n,1);
+    cout << "Kombinaciok:" << endl;
+    for (int k = 1; k <= n; ++k) {
+        Kombinacio(x, n, k, 1);
+    }
+    cout << endl << "Variaciok:" << endl;
+    for (int k = 1; k <= n; ++k) {
+        Variacio(x, n, k, 1);
+    }
+    delete[]x;
+
     return 0;
 }
